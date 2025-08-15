fb085ae7b5ba89c347b5d5d6171631cb7f744ece
Sofiia Novychenko <milakotiy@gmail.com>
2025-08-15 18:06:49 +0200

    add cache-busting to vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/lang-and-theme-switcher/",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`, // головні скрипти з хешем
+        chunkFileNames: `assets/[name]-[hash].js`, // чанки з хешем
+        assetFileNames: `assets/[name]-[hash].[ext]`, // CSS, картинки з хешем
+      },
+    },
+  },
 });
