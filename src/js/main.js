7840fa99b0f3c65aaad5be46201d5309630b6e60
Sofiia Novychenko <milakotiy@gmail.com>
2025-08-15 17:55:59 +0200

    add theme switcher that works only for light mode

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import "../css/index.css";
 
 const modeSwitches = document.querySelectorAll("[data-mode-toggle]");
+const themeSwitcher = document.querySelector("[data-theme-switcher]");
+const body = document.body;
+const themes = ["theme-blue", "theme-red", ""]; //! "" означає дефолтну тему
 
+// -------- Режим (світлий/темний) -------- //
 //зміна іконки лише якщо є use
 const updateIcon = (isDark) => {
   const iconUse = document.querySelector("[data-mode-toggle] use");
@@ -15,20 +19,47 @@ const updateIcon = (isDark) => {
   }
 };
 
-const savedMode = localStorage.getItem("theme");
+const savedMode = localStorage.getItem("mode");
+const savedTheme = localStorage.getItem("theme");
 
 //! додаємо клас 'dark' на <html> якщо в локалсторедж є "dark"
+//початковий РЕЖИМ
 if (savedMode === "dark") {
   document.documentElement.classList.add("dark");
   updateIcon(true);
 } else {
   updateIcon(false);
 }
+//початкова ТЕМА
+if (savedTheme && themes.includes(savedTheme)) {
+  body.classList.add(savedTheme);
+}
 
 modeSwitches.forEach((btn) =>
   btn.addEventListener("click", () => {
     const isDark = document.documentElement.classList.toggle("dark");
-    localStorage.setItem("theme", isDark ? "dark" : "light");
+    localStorage.setItem("mode", isDark ? "dark" : "light");
     updateIcon(isDark);
   })
 );
+
+// -------- Перемикання теми -------- //
+themeSwitcher.addEventListener("click", () => {
+  const currentTheme = themes.find((t) => body.classList.contains(t));
+  let nextTheme;
+
+  if (!currentTheme) {
+    nextTheme = themes[0];
+  } else {
+    //! просто циклічне перебирання тем по кліку
+    const currentIndex = themes.indexOf(currentTheme);
+    nextTheme = themes[(currentIndex + 1) % themes.length];
+    body.classList.remove(currentTheme);
+  }
+
+  body.classList.remove(...themes.filter(Boolean));
+  if (nextTheme) {
+    body.classList.add(nextTheme);
+  }
+  localStorage.setItem("theme", nextTheme);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@
 module.exports = {
   content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: " var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        bgPrimary: "var(--color-bg-primary)",
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
